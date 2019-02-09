6781cd789b419a41f2bb997addf11430e7091300
sergkhr <serg.khrechko@gmail.com>
2019-02-09 13:56:40 +0300

    class worked

---
+++ homework/node/server/index.js
@@ -0,0 +1,23 @@
+let express = require("express");
+let cors = require("cors");
+let mongoClient = require("mongodb").MongoClient;
+let app = express();
+
+app.use(cors());
+
+
+
+app.get("/",function(request, response) {
+    let name = request.query.name;
+    let mood = request.query.mood;
+    response.send("Hello, " + name + "! I am " + mood + " too.");
+});
+
+mongoClient.connect("mongodb://localhost", function(err, client) {
+    console.log(err);
+    let sandbox = client.db("sandbox");
+    let humans = sandbox.collection("humans");
+
+    app.listen(591);
+});
+
