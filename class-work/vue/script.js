b98789c020a9d9afb30690ea2cfa0e6527a6b372
sergkhr <serg.khrechko@gmail.com>
2019-02-23 12:27:40 +0300

    class - - vue

--- class-work/vue/script.js
+++ class-work/vue/script.js
@@ -1,6 +1,13 @@
 let vm = new Vue({
     el: "#app", // связан  именно div с id  app
     data: {
+        items: [
+            { text: "hey"},
+            {text: "how are you?"},
+            { text: "rat"}
+        ],
+        p: 5,
+
         n: 1,
         checked: true,
         message: "уругвай и парагвай",
