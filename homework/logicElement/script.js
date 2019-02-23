0811bb5076196a0c9e61f5cf9255fec026ac1fbd
sergkhr <serg.khrechko@gmail.com>
2019-02-23 15:20:05 +0300

    made logic element + some improvements in register

---
+++ homework/logicElement/script.js
@@ -0,0 +1,9 @@
+let vm = new Vue({
+    el: "#app",
+    data: {
+        both1: false,
+        both2: false,
+        colorfalse: "red",
+        colortrue: "green"
+    }
+});
--- homework/register-form/index.js
+++ homework/register-form/index.js
@@ -1,14 +1,15 @@
 let express = require("express");
 let app = express();
+let mongoClient = require("mongodb").MongoClient;
+let url = "mongodb://localhost/registred"'
 let bodyParser = require("body-parser");
 let urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 app.post("/enroll", urlencodedParser, function(request, responce){
-    app.use(express.static("D:\Sergey Khrechko\web-2\1c-web2\homework\register-form\records\records.html"));
-    responce.redirect("D:\Sergey Khrechko\web-2\1c-web2\homework\register-form\records\records.html");
+    responce.redirect("records.html");
 });
 
-app.use(express.static("/client/index.html"));
+app.use(express.static("/client"));
 
 
 app.listen(591);
