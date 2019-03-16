1606370c226a5bcbf978630db71bd12636f8d2d1
sergkhr <46621907+sergkhr@users.noreply.github.com>
2019-03-16 13:56:59 +0300

    instagr started (+ there are commands for console in indexd.js in instagr )

---
+++ instagram/server/index.js
@@ -0,0 +1,6 @@
+// npm install --global browserify
+// npm install --global watchify
+// npm install vue@2.5.16 --save-exact
+// npm install babel-core@6.26.3 babel-preset-es2015@6.24.1 babel-runtime@6.26.0 babel-plugin-transform-runtime@6.23.0 vue-template-compiler@2.5.16 vueify@9.4.1 --save-exact
+// npm install vue-router@3.0.1 vue-jwt-mongo@latest vue-resource@1.5.1  --save-exact
+// команды для консоли
