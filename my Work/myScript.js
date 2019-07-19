b31dc1bdbebecc763e43a8b2e723005e960b1b88
sergkhr <serg.khrechko@gmail.com>
2019-07-19 10:59:00 +0300

    little fixes, still no text

--- my Work/myScript.js
+++ my Work/myScript.js
@@ -1,47 +1,45 @@
-let imgButtons = document.getElementsByClassName("contactImg");
+let imgButtonsPodr = document.getElementsByClassName("contactImgPodr");
+let imgButtonsSvern = document.getElementsByClassName("contactImgSvern");
 let imgButtonConts = document.getElementsByClassName("contactImgCont");
 let Cariers = document.getElementsByClassName("contactCarier");
 let selected = [];
-for (let i = 0; i < imgButtons.length; i++){
+for (let i = 0; i < imgButtonsPodr.length; i++){
     selected.push(false);
 }
-for(let i = 0; i < imgButtons.length; i++){
-    imgButtons[i].addEventListener("click", function(){
+for(let i = 0; i < imgButtonsPodr.length; i++){
+    imgButtonsSvern[i].addEventListener("click", function(){
         //открытие блока
         if (!selected[i]){
             Cariers[i].classList.add("contactSelected");
             selected[i] = true;
-            imgButtons[i].classList.add("contactImgSelecting");
-            // if(i % 2 == 0){
-            //     imgButtons[i].src = "str_svern_nadp_grey.png";
-            // } else {
-            //     imgButtons[i].src = "str_svern_nadp_black.png";
-            // }
+            imgButtonsPodr[i].classList.add("contactImgSelecting");
+            imgButtonsSvern[i].classList.add("contactImgSelecting");
             setTimeout(function(){
-                if(i % 2 == 0){
-                    imgButtons[i].src = "str_svern_nadp_grey.png"; //по-хорошему эта строчка должна быть тут, однако в этом случаем картинка меняется только после конца операции по открытию блока (возможно картинка не успевает подгрузится), поэтому строчка скопирована выше (да, костыль).
-                } else {
-                    imgButtons[i].src = "str_svern_nadp_black.png"; //то же что и сверху написано
-                }
-                imgButtons[i].classList.remove("contactImgSelecting");
-                imgButtons[i].classList.remove("img-notSel-pos");
-                imgButtons[i].classList.add("img-sel-pos");
+                imgButtonsSvern[i].classList.remove("contactInv");
+                imgButtonsPodr[i].classList.add("contactInv");
+                imgButtonsSvern[i].classList.remove("contactImgSelecting");
+                imgButtonsPodr[i].classList.remove("contactImgSelecting");
+                imgButtonsSvern[i].classList.remove("img-notSel-pos");
+                imgButtonsPodr[i].classList.remove("img-notSel-pos");
+                imgButtonsSvern[i].classList.add("img-sel-pos");
+                imgButtonsSvern[i].classList.add("img-sel-pos");
             }, 500);
             
         } else { 
             //закрытие блока
             Cariers[i].classList.remove("contactSelected");
             selected[i] = false;
-            imgButtons[i].classList.add("contactImgSelecting");
+            imgButtonsPodr[i].classList.add("contactImgSelecting");
+            imgButtonsSvern[i].classList.add("contactImgSelecting");
             setTimeout(function(){
-                if(i % 2 == 0){
-                   imgButtons[i].src = "str_podr_nadp_grey.png";
-                } else {
-                   imgButtons[i].src = "str_podr_nadp_black.png";   
-                }
-                imgButtons[i].classList.remove("contactImgSelecting");
-                imgButtons[i].classList.remove("img-sel-pos");
-                imgButtons[i].classList.add("img-notSel-pos");
+                imgButtonsPodr[i].classList.remove("contactInv");
+                imgButtonsSvern[i].classList.add("contactInv");
+                imgButtonsSvern[i].classList.remove("contactImgSelecting");
+                imgButtonsPodr[i].classList.remove("contactImgSelecting");
+                imgButtonsPodr[i].classList.remove("img-sel-pos");
+                imgButtonsSvern[i].classList.remove("img-sel-pos");
+                imgButtonsPodr[i].classList.add("img-notSel-pos");
+                imgButtonsSvern[i].classList.add("img-notSel-pos");
             }, 500);
             
         }    
