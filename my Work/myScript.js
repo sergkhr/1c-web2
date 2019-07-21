920bae4393515b4ae3d85be6cbb472c39aa4e084
sergkhr <serg.khrechko@gmail.com>
2019-07-21 22:06:05 +0300

    uuuuuh

--- my Work/myScript.js
+++ my Work/myScript.js
@@ -3,21 +3,41 @@ let imgButtonsSvern = document.getElementsByClassName("contactImgSvern");
 let imgButtonConts = document.getElementsByClassName("contactImgCont");
 let Cariers = document.getElementsByClassName("contactCarier");
 let Texts = document.getElementsByClassName("kostil");
+let contSpaces = document.getElementsByClassName("contSpace");
+let contSpaceHeights = [];
 let selected = [];
 let cariersHeight = [];
 let textsHeights = [];
+let cariersBigHeights = [];
+
+for (let i = 0; i < contSpaces.length; i++){
+    contSpaceHeights.push(getComputedStyle(contSpaces[i]).height);
+}
 
 for(let i = 0; i < Texts.length; i++){
-    textsHeights.push(getComputedStyle(Texts[i]).height);
-    console.log(getComputedStyle(Texts[i]).height);
+    textsHeights.push( Number(getComputedStyle(Texts[i]).height.replace("px", "")) + "px");
     Texts[i].classList.add("contactInfSvern");
     Texts[i].classList.remove("contactInfBig");
-    Texts[i].classList.remove("contactInv");
+    setTimeout(function(){
+        Texts[i].classList.remove("contactInv");
+    },250);
+    if( i == 2) {
+        textsHeights[i] =  Number(textsHeights[i].replace("px","")) + 25 + "px";
+    }
 }
 
 for(let i = 0; i < Cariers.length; i++){
-    cariersHeight.push(getComputedStyle(Cariers[i]).height);
-    Cariers[i].style.height = getComputedStyle(Cariers[i]).height;
+    cariersHeight.push( Number(getComputedStyle(Cariers[i]).height.replace("px", "")) + "px");
+    Cariers[i].style.height = cariersHeight[i];
+    console.log(cariersHeight[i]);
+    if(i == 2){
+        cariersHeight[i] = Number(getComputedStyle(Cariers[i]).height.replace("px", "")) + 45 + "px";
+        Cariers[i].style.height = cariersHeight[i];
+    }
+}
+
+for (let i = 0; i < Cariers.length; i++){
+    cariersBigHeights.push(Number(cariersHeight[i].replace("px", "")) + Number(textsHeights[i].replace("px", "")) + "px");
 }
 
 for (let i = 0; i < imgButtonsPodr.length; i++){
@@ -27,7 +47,7 @@ for(let i = 0; i < imgButtonsPodr.length; i++){
     imgButtonsSvern[i].addEventListener("click", function(){
         //открытие блока
         if (!selected[i]){
-            Cariers[i].style.height = Number(cariersHeight[i].replace("px", "")) + Number(textsHeights[i].replace("px", "")) + 10 + "px";
+            Cariers[i].style.height = cariersBigHeights[i];
             Texts[i].classList.remove("contactInfSvern");
             Texts[i].classList.add("contactInfBig");
             Texts[i].style.height = textsHeights[i];
